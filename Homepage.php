82ad37a0ae5b6105e55901924c2d26d5774bf908
Jishan175 <mdzillur175@gmail.com>
2017-11-16 21:54:24 +0600

    New Homepage Added

---
+++ Homepage.php
@@ -0,0 +1,27 @@
+<!Doctype html>
+<html>
+	<head>
+		<title>Webo Motors</title>
+	</head>
+	<body>
+		<table width="100%" border="0" cellspacing="0"  >
+			<tr bgcolor="black">
+				<td>&nbsp;&nbsp;&nbsp;</td>
+				<td><?phpecho ""?><a href="Homepage.php"><img src="Image/mainlogo.gif" alt="Webo Motors" width="150" align="left"><a></td>
+				<td align="right">
+					<table>
+						<tr>
+							<td><input type="text" style="padding: 8px;" size="60" valign="center"/></td>
+							<td>
+								<button valign="center">
+									<font size="5">Search</font><img src="Image/icon1.png" height="25" alt="Save icon">
+								</button>
+							</td>
+						</tr>
+					</table>
+				</td>
+				<td>&nbsp;&nbsp;&nbsp;</td>
+			</tr>
+		</table>
+	</body>
+</html>
