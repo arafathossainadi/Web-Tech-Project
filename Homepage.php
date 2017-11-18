0c2def64277cf2dd3a81831e068c7face50e00f9
arafathossainadi <muanakaa@gmail.com>
2017-11-18 19:51:49 +0600

    update

--- Homepage.php
+++ Homepage.php
@@ -5,18 +5,18 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<body>
-		<table width="100%" border="0" cellspacing="0"  >
+		<table width="1170" align="center" border="0" cellspacing="0"  >
 			<tr bgcolor="black">
-				<td>
-					<table>
+				<td width="200">
+					<table border="0">
 						<tr>
 							<td>&nbsp;&nbsp;&nbsp;</td>
 							<td><?phpecho ""?><a href="Homepage.php"><img src="Image/mainlogo.gif" alt="Webo Motors" width="150" align="left"><a></td>
 						</tr>
 					</table>
 				</td>
-				<td align="right">
-					<table>
+				<td colspan="3" align="right">
+					<table border="0">
 						<tr>
 							<td><input type="text" placeholder="Search Your Product Here" name="search_bar" style="padding: 8px;" size="60" valign="center"/></td>
 							<td>
@@ -24,12 +24,11 @@
 									<font size="5">Search</font><img src="Image/icon1.png" height="25" alt="Save icon">
 								</button>
 							</td>
-							<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
 							<td><a><img src="Image/cart.png" height="55" alt="cart_icon"></a></td>
+							<td width="10"></td>
 						</tr>
 					</table>
 				</td>
-				<td>&nbsp;&nbsp;&nbsp;</td>
 			</tr>
 			<tr bgcolor="#d1f2eb">
 				<td colspan="3" align="right">
@@ -38,17 +37,17 @@
 							<td><a style="text-decoration:none;" href="Homepage.php"><font size="5"><b>Home</b></font></a></td>
 							<td><a href="Homepage.php"><img src="Image/home.png" height="25" alt="home_icon"></a></td>
 							<td>&nbsp;<font size=4"><b>|&nbsp;&nbsp;</b></font></td>
-							<td><a style="text-decoration:none;"><font size="5"><b>Login</b></font></a></td>
-							<td><a><img src="Image/login.png" height="25" alt="login_icon"></a></td>
-							<td>&nbsp;<font size=4"><b>|&nbsp;&nbsp;</b></font></td>
-							<td><a style="text-decoration:none;"><font size="5"><b>Sign Up</b></font></a></td>
-							<td><a><img src="Image/signup.png" height="25" alt="signup_icon"></a></td>
-							<td>&nbsp;<font size=4"><b>|&nbsp;&nbsp;</b></font></td>
 							<td><a style="text-decoration:none;"><font size="5"><b>About</b></font></a></td>
 							<td><a><img src="Image/about.png" height="25" alt="about_icon"></a></td>
 							<td>&nbsp;<font size=4"><b>|&nbsp;&nbsp;</b></font></td>
 							<td><a style="text-decoration:none;"><font size="5"><b>Contact</b></font></a></td>
 							<td><a><img src="Image/contact.png" height="25" alt="contact_icon"></a></td>
+							<td>&nbsp;<font size=4"><b>|&nbsp;&nbsp;</b></font></td>
+							<td><a style="text-decoration:none;"><font size="5"><b>Login</b></font></a></td>
+							<td><a><img src="Image/login.png" height="25" alt="login_icon"></a></td>
+							<td>&nbsp;<font size=4"><b>|&nbsp;&nbsp;</b></font></td>
+							<td><a style="text-decoration:none;"><font size="5"><b>Sign Up</b></font></a></td>
+							<td><a><img src="Image/signup.png" height="25" alt="signup_icon"></a></td>
 							<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
 						</tr>
 					</table>
