eb7c93039006dd84ba698cb303b4c618c7673e80
arafathossainadi <muanakaa@gmail.com>
2017-11-30 21:47:56 +0600

    Commit By Adi at 09:47 PM on 30/11.

--- Dashboard.php
+++ Dashboard.php
@@ -1,92 +1,115 @@
-<!Doctype html>
-<html>
-	<head>
-		<title>Administrator</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	</head>
-	<body>
-		<table width="100%" align="center" border="0" cellspacing="0">
-			<tr bgcolor="black">
-				<td width="200">
-					<table border="0">
-						<tr>
-							<td>&nbsp;&nbsp;&nbsp;</td>
-							<td><?phpecho ""?><a href="index.html"><img src="Image/mainlogo.gif" alt="Webo Motors" width="150" align="left"><a></td>
-						</tr>
-					</table>
-				</td>
-				<td colspan="3" align="right">
-					<table border="0">
-						<tr>
-							<td><font size="5" color="white">Administrator</font>&nbsp;&nbsp;&nbsp;&nbsp;</td>
-						</tr>
-						<tr>
-	
-							
-						</tr>
-						
-					</table>
-				</td>
-			</tr>
-			<tr height="50" bgcolor="#8dcaa1">
-				<td  align="left">&nbsp;&nbsp;</td>
-				<td align="right" colspan="2">
-					<table>
-						<tr>
-							<td align="center"><image src="Image/message_icon.png" height="30" alt="icon"/>&nbsp;&nbsp;&nbsp;</td>
-							<td align="center"><image src="Image/notification.png" height="35" alt="icon"/></td>
-							<td align="center" valign="center">&nbsp;<font size="5">Log Out&nbsp;
-							<image src="Image/logout.jpg" height="25" alt="icon"/></font>&nbsp;&nbsp;&nbsp;&nbsp;</td>
-						</tr>
-						<tr>
-							<td align="center"><font  size="4">Messages&nbsp;&nbsp;&nbsp;</font></td>
-							<td align="center"><font  size="4">Notification</font></td>
-						</tr>
-					</table>
-				</td>
-			</tr>
-			<tr height="400" bgcolor="#7f788e">
-				<td align="right" valign="top" bgcolor="#7f788e">
-					<table>
-						<tr>
-							<td valign="top">&nbsp;&nbsp;&nbsp;&nbsp;</td>
-							<td valign="top">&nbsp;&nbsp;&nbsp;&nbsp;</td>
-						</tr>
-						<tr>
-							<td valign="top"><font size="5"><a>&nbsp;&nbsp;Dashboard</a></font><br/></td>
-						</tr>
-						<tr>
-							<td valign="top"><font size="5"><a>&nbsp;&nbsp;Product Information</a></font><br/></td>
-						</tr>
-						<tr>
-							<td valign="top"><font size="5"><a>&nbsp;&nbsp;Employee Information</a></font><br/></td>
-						</tr>
-						<tr>
-							<td valign="top"><font size="5"><a>&nbsp;&nbsp;Edit Your Profile</a></font><br/></td>
-						</tr>
-						<tr>
-							<td valign="top"><font size="5"><a>&nbsp;&nbsp;Customer Information</a></font><br/></td>
-						</tr>
-						<tr>
-							<td valign="top"><font size="5"><a>&nbsp;&nbsp;Sales History</a></font><br/></td>
-						</tr>
-						<tr>
-							<td valign="top"><font size="5"><a>&nbsp;&nbsp;Email</a></font><br/></td>
-						</tr>
-					</table>
-				</td>
-				<td colspan="2" bgcolor="#788e87">&nbsp;&nbsp;<td>
-			</tr>
-			<tr bgcolor="black" height="50" >
-				<td colspan="3">
-					<table border="0">
-						<tr>
-							<td  width="900"  align="center" valign="center"><font color="white">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Copyright©2017</td>
-							<td  width="300" align="center" valign="center"><font color="white">Terms and Conditions&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Privacy Policy</td>
-						</tr>
-					</table>
-				</td>
-			</tr>
-		</table>
-	</body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ADMIN</title>
+</head>
+<body>
+	<table width="100%" border="0" cellspacing="0" align="center">
+		<tr bgcolor="black">
+			<td>
+				<table width="1170" align="center" border="0" cellspacing="0">
+					<tr><td height="10"></td></tr>
+					<tr>
+						<td width="25"></td>
+						<td height="160"><a href="index.html"><img src="Image/mainlogo.gif" alt="Webo Motors" width="150" align="left"><a></td>
+						<td align="right"><font size="10" color="white">ADMIN PANEL</font></td>
+					</tr>
+					<tr><td height="10"></td></tr>
+				</table>
+			</td>
+		</tr>
+		<tr bgcolor="8DCAA1">
+			<td>
+				<table width="1170" align="center" border="0" cellspacing="0">
+					<tr><td height="5"></td></tr>
+					<tr>
+						<td align="right">
+							<td align="right" valign="center">&nbsp;
+								<font size="5">Log Out
+									<image src="Image/logout.jpg" height="25" alt="icon"/>
+								</font>
+							</td>
+						</td>
+					</tr>
+					<tr><td height="5"></td></tr>
+				</table>
+			</td>
+		</tr>
+		<tr bgcolor="#C0C0C0">
+			<td >
+				<table width="1170" align="center" border="0" cellspacing="0">
+					<tr>
+						<td  valign="top" bgcolor="#C0C0C0">
+							<table border="0" bgcolor="7F788E">
+								<tr><td height="10"></td></tr>
+								<tr>
+									<td width="10"></td>
+									<td  width="230">
+										<font size="5">Dashboard</font>
+									</td>
+								</tr>
+								<tr><td height="10"></td></tr>
+								<tr>
+									<td width="10"></td>
+									<td >
+										<font size="5">Vehicles Information</font>
+									</td>
+								</tr>
+								<tr><td height="10"></td></tr>
+								<tr>
+									<td width="10"></td>
+									<td >
+										<font size="5">Employee Information</font>
+									</td>
+								</tr>
+								<tr><td height="10"></td></tr>
+								<tr>
+									<td width="10"></td>
+									<td >
+										<font size="5">Customer Information</font>
+									</td>
+								</tr>
+								<tr><td height="10"></td></tr>
+								<tr>
+									<td width="10"></td>
+									<td >
+										<font size="5">Sales History</font>
+									</td>
+								</tr>
+								<tr><td height="10"></td></tr>
+								<tr>
+									<td width="10"></td>
+									<td >
+										<font size="5">Email</font>
+									</td>
+								</tr>
+								<tr><td height="10"></td></tr>
+								<tr>
+									<td width="10"></td>
+									<td >
+										<font size="5">Edit Profile</font>
+									</td>
+								</tr>
+								<tr><td height="10"></td></tr>
+							</table>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<td bgcolor="black">
+				<table width="1170" align="center" border="0" cellspacing="0">
+					<tr><td height="10"></td></tr>
+					<tr>
+						<td colspan="2" align="center" valign="center"><font color="white">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Copyright©2017</td>
+						<td align="center" valign="center" width="300"><font color="white">Terms and Conditions&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Privacy Policy</td>
+					</tr>
+					<tr><td height="10"></td></tr>
+				</table>
+			</td>
+		</tr>
+	</table>
+</body>
 </html>
